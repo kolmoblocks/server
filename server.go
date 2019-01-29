6dd3debbd49dda745387568cbf3014453412e8fc
Dmitry Borzov <d@borzov.ca>
2019-01-29 15:13:09 -0500

    Fix uploading functionality

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"encoding/json"
+
 	"github.com/gorilla/mux"
 )
 
@@ -9,6 +11,10 @@ type Manifest struct {
 	Mime string `json:"MIME"`
 	Cids Hashes `json:"cids"`
 	Size int    `json:"size"`
+}
+
+func (m Manifest) ToJSON() ([]byte, error) {
+	return json.MarshalIndent(&m, "", "\t")
 }
 
 //Hashes struct for marshaling/unmarshaling json manifest
@@ -23,7 +29,5 @@ func NewRouter() *mux.Router {
 	r.HandleFunc("/upload_json", uploadJSON)
 	r.HandleFunc("/search", search).Methods("GET")
 	r.HandleFunc("/raw/{ref}", serveRaw).Methods("GET")
-	// r.PathPrefix("/").Handler(http.FileServer(http.Dir("public")))
-
 	return r
 }
